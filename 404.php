7b2f18f66094e0f74fab9b1bf1404a621259f296
John Heimkes IV <bigjohn@Whitey.local>
2010-01-18 02:40:15 -0600

    adding templates

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+	<div id="content">
+
+    <?php include 'leftSidebar.php' ?>
+
+		<h2 class="center">Sorry but we could not find what you were looking for. It could possibly be lost in space.</h2>
+
+    <?php include 'rightSidebar.php' ?>
+
+	</div>
+
+<?php get_footer(); ?>
